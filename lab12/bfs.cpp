9faf6d8abd35654b61b0269327b84c2cf672394d
bhagavath <lordcodeking@gmail.com>
2022-04-22 17:23:40 +0530

    dfs ans bfs iterative

---
+++ lab12/bfs.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+
+int main()
+ { 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int v,p,c;
+    cin>>v;
+    vector<vector<int>> adjlist(v);
+    vector<int> visited(v,false);
+
+    while(cin>>p && cin>>c)
+      adjlist[p].push_back(c);
+  queue<int> crnt;
+  crnt.push(0);
+  while(!crnt.empty())
+  {
+  	int node = crnt.front();
+  	crnt.pop();
+
+  	if(visited[node] == false)
+  	{
+  		cout<<node<<" ";
+  		visited[node]= true;
+  	}
+
+  	int l=adjlist[node].size();
+  	for(int i=0;i<l;++i)
+  	{
+  		if(visited[adjlist[node][i]] == false)
+  			crnt.push(adjlist[node][i]);
+  	}
+
+  }
+
+
+
+   
+    
+    return 0;
+ } 
+
+
+ // take a random node as a root in graph maintain a queue to store nodes in bfs order 
+ //we take front of queue when acessing a node and add add new nodes at the back of queue 
+ // so in queue elements will arrenged acording to order of there level  
+ // and arrary to know if each node is visited or not to avoid cycles in graph
+ // if not visted print and make it visted 
+ //and push all non visited childs of curent node and continue till the stack is empty 
+ // time complexity = O(V+E)
+// auxilary space = O(V)
+
